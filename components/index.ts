9589840cd77a9f73ae61f4f2201dada2610c0f20
Huy Tran <tranthanhhuy2207@gmail.com>
2023-07-29 15:32:49 +0700

    Create basic view

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+import CustomFilter from './CustomFilter'
+import SearchBar from './SearchBar'
+import SearchManufacturer from './SearchManufacturer'
+import NavBar from './NavBar'
+import Footer from './Footer'
+
+export {
+  Hero,
+  CustomButton,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  NavBar,
+  Footer
+}
